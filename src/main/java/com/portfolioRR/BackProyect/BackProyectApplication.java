2afb90fe7afcd08044c52586314a12ab7d335f07
renzo <renzojrq1990@gmail.com>
2022-12-04 14:42:33 -0300

    comenzando el Back-end

---
+++ src/main/java/com/portfolioRR/BackProyect/BackProyectApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioRR.BackProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioRR/BackProyect/BackProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioRR.BackProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
