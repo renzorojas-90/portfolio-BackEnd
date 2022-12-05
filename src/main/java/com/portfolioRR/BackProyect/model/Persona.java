6703c095bc2ab95dbd14ebe0801abf051318657b
renzo <renzojrq1990@gmail.com>
2022-12-05 16:08:11 -0300

    Creando la clase persona, mapeandola, conectandola a la BD

--- src/main/java/com/portfolioRR/BackProyect/model/Persona.java
+++ src/main/java/com/portfolioRR/BackProyect/model/Persona.java
@@ -5,10 +5,26 @@
  */
 package com.portfolioRR.BackProyect.model;
 
-/**
- *
- * @author Renzo
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
 public class Persona {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+int id;
+String nombre;
+String apellido;
+int edad;
+String fotoPerfil;
+String tituloDesarrollador;
+String usuario;
+String password;
     
 }
