35b2e748bc0a53ba8012e96bf48af3b58fd74159
renzo <renzojrq1990@gmail.com>
2023-01-13 00:36:35 -0300

    subida1.1

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM amazoncorretto:11-alpine-jdk
+
+MAINTAINER Renzo
+
+COPY target/BackProyect-0.0.1-SNAPSHOT.jar BackProyect-0.0.1-SNAPSHOT.jar
+
+ENTRYPOINT ["java","-jar","/BackProyect-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/portfolioRR/BackProyect/BackProyectApplication.java
+++ src/main/java/com/portfolioRR/BackProyect/BackProyectApplication.java
@@ -3,10 +3,14 @@ package com.portfolioRR.BackProyect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
+@Configuration
 public class BackProyectApplication {
 
 	public static void main(String[] args) {
