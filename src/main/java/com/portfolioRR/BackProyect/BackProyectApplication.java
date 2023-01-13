f5fb3a258c105fab00246015c500292feb44d459
renzo <renzojrq1990@gmail.com>
2023-01-13 01:52:15 -0300

    subida1.2

--- src/main/java/com/portfolioRR/BackProyect/BackProyectApplication.java
+++ src/main/java/com/portfolioRR/BackProyect/BackProyectApplication.java
@@ -3,14 +3,10 @@ package com.portfolioRR.BackProyect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-@Configuration
 public class BackProyectApplication {
 
 	public static void main(String[] args) {
